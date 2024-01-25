694fe0cbfa44a6dd3060535107dfd3ed8aca9f46
Don Bhanuka <bhanukamax@gmail.com>
2024-01-25 23:39:11 +0530

    monkeyfy

---
+++ src/hooks.ts
@@ -0,0 +1,15 @@
+import { useEffect } from 'react'
+
+export function useFocusRefOnBodyClick(ref: React.RefObject<HTMLElement>) {
+    useEffect(() => {
+        const handleBodyClick = (e: MouseEvent) => {
+            if (ref.current && !ref.current.contains(e.target as Node)) {
+                ref.current.focus()
+            }
+        }
+        document.body.addEventListener('click', handleBodyClick)
+        return () => {
+            document.body.removeEventListener('click', handleBodyClick)
+        }
+    }, [ref])
+}
