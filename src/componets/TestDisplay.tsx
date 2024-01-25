fe886c405c1287b8a5b972e0f0e63802c3bb2fde
Don Bhanuka <bhanukamax@gmail.com>
2024-01-25 22:14:14 +0530

    added prettier and pretify everything

--- src/componets/TestDisplay.tsx
+++ src/componets/TestDisplay.tsx
@@ -1,32 +1,41 @@
 interface Props {
-    userText: string;
+  userText: string;
   test: string;
   onClick: () => void;
 }
 export default function TestDisplay({ test, userText, onClick }: Props) {
-    return (
-        <div onClick={() => onClick()}>
-            <h2>
-                {test.split('').map((letter: string, index: number) => {
-                    let style;
-                    let correct = true;
-                    if (index < userText.length) {
-                      correct = letter !== userText[index]
-                      style = correct ? { color: 'var(--default-error)', textDecoration: 'var(default-error-bg)', backgroundColor: "var(--default-error-bg)"} : { color: 'var(--default-text)' };
-                    }
+  return (
+    <div onClick={() => onClick()}>
+      <h2>
+        {test.split("").map((letter: string, index: number) => {
+          let style;
+          let correct = true;
+          if (index < userText.length) {
+            correct = letter !== userText[index];
+            style = correct
+              ? {
+                  color: "var(--default-error)",
+                  textDecoration: "var(default-error-bg)",
+                  backgroundColor: "var(--default-error-bg)",
+                }
+              : { color: "var(--default-text)" };
+          }
 
+          let hasCursor = index === userText.length;
+          return (
+            <span
+              key={index}
+              style={style}
+              className={hasCursor ? "cursor letter" : "letter"}
+            >
+              {letter}
+            </span>
+          );
+        })}
+      </h2>
 
-                  let hasCursor = index === userText.length;
-                    return (
-                        <span key={index} style={style} className={hasCursor ? 'cursor letter' : 'letter'}>
-                            {letter}
-                        </span>
-                    )
-                })}
-            </h2>
-
-            {/* <h3>{userText}</h3>
+      {/* <h3>{userText}</h3>
             <h3>{test}</h3> */}
-        </div>
-    )
+    </div>
+  );
 }
