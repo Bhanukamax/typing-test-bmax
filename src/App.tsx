b6bd7df39ff9a52ae0c1377957c860f8b2dfe5f3
Don Bhanuka <bhanukamax@gmail.com>
2024-01-26 21:06:22 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,44 @@
+import { useState } from 'react';
 import { IoCog } from 'react-icons/io5';
 import './App.css';
+import Settings from './componets/Settings';
 import TypingTest from './componets/TypingTest';
+const TEST_SIZE = 10;
+
+enum AppScreen {
+    TEST,
+    SETTINGS,
+}
 
 function App() {
+    const [screen, setScreen] = useState(AppScreen.TEST);
+
     return (
         <div className="App">
             <div className="main">
                 <div className="header-line">
                     <h1>Typing Test</h1>
                     <button className="border-0 flex bg-none color-sub hover:color-main pointer">
-                        <IoCog size="20" />
+                        <IoCog
+                            size="20"
+                            onClick={() =>
+                                setScreen(
+                                    screen === AppScreen.TEST
+                                        ? AppScreen.SETTINGS
+                                        : AppScreen.TEST
+                                )
+                            }
+                        />
                     </button>
                 </div>
-                <TypingTest />
+                {(() => {
+                    switch (screen) {
+                        case AppScreen.TEST:
+                            return <TypingTest wordCount={10} />;
+                        case AppScreen.SETTINGS:
+                            return <Settings />;
+                    }
+                })()}
             </div>
         </div>
     );
