fd18b0289473d0358474a3116120f741a22744b9
Don Bhanuka <bhanukamax@gmail.com>
2024-01-27 00:17:20 +0530

    fix

--- src/componets/TypingTest.tsx
+++ src/componets/TypingTest.tsx
@@ -191,7 +191,7 @@ export default function TypingTest({ wordCount }: Props) {
                 onChange={(e) => setUserText(e.target.value)}
                 onBlur={handleOnBlur}
             />
-            <button onClick={newTest} className="button">
+            <button onClick={newTest} className="button rounded-full">
                 New Test &nbsp; <IoReloadCircle size={20} />{' '}
             </button>
         </>
